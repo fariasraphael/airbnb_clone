150a6d014556326162d83a797030b2082c992e2e
Raphael Farias <farias.raphael@gmail.com>
2024-01-06 19:07:12 -0300

    add test create new user with a alredy existing email

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -22,6 +22,28 @@ class UsersTest < ApplicationSystemTestCase
     # save_and_open_screenshot
   end
 
+  test 'creating a new user with already existing email' do
+    visit root_path
+    assert_selector '#navbarDropdown', visible: true
+    assert_selector '.fa-solid.fa-bars', visible: true
+
+    find('.avatar').click
+    assert_selector 'a', text: 'Sign up'
+
+    click_on 'Sign up'
+
+    fill_in 'user_email', with: 'farias.raphael@gmail.com'
+    fill_in 'user_password', with: '987654'
+    fill_in 'user_password_confirmation', with: '987654'
+    assert_equal new_user_registration_path, page.current_path
+
+    click_on 'Sign up'
+    assert_text 'Please review the problems below:'
+    assert_equal new_user_registration_path, page.current_path
+    assert_selector('.invalid-feedback', visible: true)
+    # save_and_open_screenshot
+  end
+
   test "log out a user" do
     login_as users(:raphael)
     visit root_path
@@ -55,6 +77,5 @@ class UsersTest < ApplicationSystemTestCase
     assert_text 'Signed in successfully.'
 
     # save_and_open_screenshot
-
   end
 end
