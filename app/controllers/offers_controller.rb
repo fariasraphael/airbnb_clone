d5d5968143a4d3f57510200604bbd07c2ed2ff25
Raphael Farias <farias.raphael@gmail.com>
2024-01-13 14:09:12 -0300

    add actions new, crate and show on offers controler

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,20 +1,27 @@
 class OffersController < ApplicationController
   def new
     @offer = Offer.new
+    @user = User.find(params[:user_id])
   end
 
   def create
+    @user = User.find(params[:user_id])
     @offer = Offer.new(offer_params)
+    @offer.user = @user
     if @offer.save
-      redirect_to root_path
+      redirect_to offer_path(@offer)
     else
       render :new
     end
   end
 
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
   private
 
   def offer_params
-    params.require(:offer).permit(:title, :description)
+    params.require(:offer).permit(:title, :description, :address, :price, :number_of_gests)
   end
 end
