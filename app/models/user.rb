0446d750e7d392a740d83b41d48848d541a8c745
Raphael Farias <farias.raphael@gmail.com>
2024-01-13 14:10:43 -0300

    add association between offer and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :offers
   def fullname
     "#{first_name.capitalize} #{last_name.capitalize}"
   end
