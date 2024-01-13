3012e8897d28cd76e043e166fee89dcfef23017e
Raphael Farias <farias.raphael@gmail.com>
2024-01-13 14:13:16 -0300

    add user reference to offer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_12_021813) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_152923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_021813) do
     t.float "price"
     t.integer "number_of_guests"
     t.string "address"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_021813) do
 
   add_foreign_key "bookings", "offers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "offers", "users"
 end
